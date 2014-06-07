bcafe4867ba898e2bdf60d36929a130a1f5ee92d
matseng <mats.engstrom@gmail.com>
2014-06-08 00:19:08 +0800

    Add dummmy tests & travis.yml

--- test/test.js
+++ test/test.js
@@ -1,3 +1,21 @@
 'use strict';
 /*jslint node: true */
 
+var expect=require('chai').expect;
+var troll=require('../lib/troll-avg');
+
+describe('troll-avg', function() {
+
+  // ------------------------------------------------------------------------
+  describe('#dummy', function() {
+    it('1 should equal 1', function() {
+      expect(1).to.equal(1);
+    });
+  });
+
+
+
+});
+
+
+
